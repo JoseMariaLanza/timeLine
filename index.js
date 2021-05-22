e22f6e42d5186dd33eb926c020650474f2877127
José María Lanza <lanza.josemaria@gmail.com>
2021-05-22 13:21:16 -0300

    joined popover data

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
 $(document).ready(function () {
     $('[data-toggle="popover"]').popover({
         title: popoverTitle,
+        content: popoverContent,
         html: true,
         placement: 'bottom',
         trigger: 'hover',
-        content: popoverContent
     })
     timeLine()
 });
 
+var repeatedElements;
+
+function popoverTitle() {
+
+    let element = $(this)
+    let tag = document.getElementById(element.attr('id'))
+    let title = tag.parentNode.lastElementChild.lastElementChild.innerHTML
+
+    repeatedElements.forEach(rep => {
+        if (rep.repeatedElementIndexes === parseInt(tag.id)) {
+            tag = document.getElementById(rep.id)
+            title = tag.parentNode.lastElementChild.lastElementChild.innerHTML + ' - ' + rep.title
+        }
+    });
+
+    return title
+}
+
 function popoverContent() {
     let element = $(this)
     let tag = document.getElementById(element.attr('id'))
-    return tag.innerHTML
-}
+    let content = tag.innerHTML
 
-function popoverTitle() {
-    let element = $(this)
-    let tag = document.getElementById(element.attr('id'))
+    repeatedElements.forEach(rep => {
+        if (rep.repeatedElementIndexes === parseInt(tag.id)) {
+            console.log(rep.id)
+            tag = document.getElementById(rep.id)
+            content = tag.innerHTML
+        }
+    });
 
-    // // Style (refactorizar)
-    // tag.style.outline = 'none'
 
-    let parentNode = tag.parentNode
-    return parentNode.lastElementChild.lastElementChild.innerHTML
+    return content
 }
 
 function timeLine() {
     let timeLineWidth = document.getElementById('timeline').offsetWidth;
 
-    const firstDateElement = document.getElementById(1)
-    firstDateElement.style.left = "0px"
+    const firstElement = document.getElementById(1)
+    firstElement.style.left = "0px"
 
     const lastDateElement = document.getElementById(8)
 
-    const totalDays = getDaysBetweenElements(parseInt(firstDateElement.id), parseInt(lastDateElement.id))
+    const totalDays = getDaysBetweenElements(parseInt(firstElement.id), parseInt(lastDateElement.id))
 
-    for (let i = 2; i <= 8; i++) {
-        let actualDateElement = document.getElementById(i)
+    const dates = new Array()
+    repeatedElements = new Array()
 
-        let left = (timeLineWidth * getDaysBetweenElements(parseInt(firstDateElement.id), parseInt(actualDateElement.id))) / totalDays
+    for (let i = 0; i <= 7; i++) {
+        let currentElement = document.getElementById(i + 1)
 
-        left = (i === 8) ? left -= actualDateElement.offsetWidth : left
-        actualDateElement.style.left = left + "px"
+        let left = (timeLineWidth * getDaysBetweenElements(parseInt(firstElement.id), parseInt(currentElement.id))) / totalDays
+
+        left = (i === 7) ? left -= currentElement.offsetWidth : left
+        currentElement.style.left = left + "px"
+        dates.push(currentElement.lastElementChild.innerHTML)
+
+        const dateIndex = searchRepeatedDates(dates, currentElement.lastElementChild.innerHTML, i)
+        if (dateIndex) {
+
+            repeatedElements.push({
+                id: dateIndex,
+                title: currentElement.parentNode.lastElementChild.lastElementChild.innerHTML,
+                value: currentElement.lastElementChild.innerHTML,
+                repeatedElementIndexes: parseInt(currentElement.id),
+            })
+
+        }
+
     }
 }
 
@@ -59,11 +94,17 @@ function formatDate(id) {
     return new Date(new Date().getFullYear(), dayMonth[1] - 1, dayMonth[0])
 }
 
+function searchRepeatedDates(dates, currentDate, index) {
+    if (dates.indexOf(currentDate) !== index) {
+        return dates.indexOf(currentDate) + 1
+    }
+}
+
 function debounce(func, time) {
     var time = time || 100
     var timer
-    return function(event) {
-        if(timer) clearTimeout(timer)
+    return function (event) {
+        if (timer) clearTimeout(timer)
         timer = setTimeout(func, time, event)
     }
 }
